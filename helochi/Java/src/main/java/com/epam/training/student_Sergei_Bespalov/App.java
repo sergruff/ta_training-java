8363f140a077c96f9cf1f8dee37cd5b80089098f
sergruff <sergruff@gmail.com>
2023-02-10 22:17:39 +0100

    Maven (I can win!)

---
+++ helochi/Java/src/main/java/com/epam/training/student_Sergei_Bespalov/App.java
@@ -0,0 +1,17 @@
+package com.epam.training.student_Sergei_Bespalov;
+
+/**
+ * Hello CI!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+    
+    public static void doNothing() {
+    	// do nothing
+    }
+}
