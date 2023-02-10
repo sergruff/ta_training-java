f624a2b2168f94b96220715eb89c57a0242d00c2
sergruff <sergruff@gmail.com>
2023-02-10 02:08:11 +0100

    MeetAStranger

---
+++ meet-a-stranger/src/main/java/com/epam/training/student_Sergei_Bespalov/MeetAStranger.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_Sergei_Bespalov;
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        System.out.println("Hello, " + new Scanner(System.in).nextLine());
+    }
+}
